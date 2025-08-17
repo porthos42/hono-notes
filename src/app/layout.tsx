fab6b370ad0959a98cef6e2ce85e79fe8b59c040
grootd <wavestorm42@gmail.com>
2025-08-16 15:13:00 -1000

    Build Sidebar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
 
 export const metadata: Metadata = {
   title: "HONO Notes",
@@ -23,8 +25,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
-          {children}
+          <SidebarProvider>
+            <AppSidebar />
+
+            <div className="flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
 
           <Toaster />
         </ThemeProvider>
---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,51 @@
+import { getUser } from "@/auth/server";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+} from "@/components/ui/sidebar";
+import SidebarGroupContent from "./SidebarGroupContent";
+import { prisma } from "@/db/prisma";
+import { Note } from "@prisma/client";
+import Link from "next/link";
+
+async function AppSidebar() {
+  const user = await getUser();
+
+  let notes: Note[] = [];
+
+  if (user)
+    notes = await prisma.note.findMany({
+      where: {
+        authorId: user.id,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+
+  return (
+    <Sidebar>
+      <SidebarContent className="custom-scrollbar">
+        <SidebarGroup>
+          <SidebarGroupLabel className="mt-2 mb-2 text-lg">
+            {user ? (
+              "Your notes"
+            ) : (
+              <p>
+                <Link href="/Login" className="underline">
+                  Login
+                </Link>{" "}
+                to see your nodes
+              </p>
+            )}
+          </SidebarGroupLabel>
+          {user && <SidebarGroupContent notes={notes} />}
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
